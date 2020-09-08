d8fcebd61a9ba830ea05d17c55078adb1718e81e
Christerle Nounoute <christerlesn@gmail.com>
2020-09-08 01:58:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
-def reverse_each_word(string)
-  string_split = string.split("")
-  reversed = []
-  string.size.times {reversed <<  string_split.pop}
-  reversed.join
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
 end
